41bd006d8b127a02e3970069f3aaefebee05389f
thiagomussa <thiagosilva_stz@hotmai.com>
2023-12-30 12:29:06 -0800

    post chat engine

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,16 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  try {
+    const r = await axios.put (
+      "https://api.chatengine.io/users/", 
+     {  username: username, secret: username, first_name: username},
+     {  headers: {"private-key}": "93b4813b-78ea-4123-bcda-0b14519a8682" }}
+        );
+    return res.status(r.status).json(r.data)
+}   catch (e) {
+    return res.status(e.response).json(e.response.data);
+    } 
 });
 
 app.listen(3001); 
