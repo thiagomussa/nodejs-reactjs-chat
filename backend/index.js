2de02cad4959f84ee4dbb493b5b29c3e241eeb80
thiagomussa <thiagosilva_stz@hotmai.com>
2023-12-30 13:17:57 -0800

    frontend component

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
 
 const app = express();
 app.use(express.json());
@@ -11,7 +13,7 @@ app.post("/authenticate", async (req, res) => {
     const r = await axios.put (
       "https://api.chatengine.io/users/", 
      {  username: username, secret: username, first_name: username},
-     {  headers: {"private-key}": "93b4813b-78ea-4123-bcda-0b14519a8682" }}
+     {  headers: { "private-key": "93b4813b-78ea-4123-bcda-0b14519a8682" }}
         );
     return res.status(r.status).json(r.data)
 }   catch (e) {
