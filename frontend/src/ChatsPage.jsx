db9d9707162964c386a0806ec3fb01e6390cfe21
thiagomussa <thiagosilva_stz@hotmai.com>
2024-01-01 14:25:39 -0800

    changing ChatsPage to a more attractive UI

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+    <div style={{ height: '100vh' }}>
+         <PrettyChatWindow
+            projectId='b1570029-b96a-4227-93c6-6e3ddaf79bb4'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{ height: '100%' }}   
+        />
+    </div>)          
 }
 
 export default ChatsPage 
